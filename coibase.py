c5638a168f2df8517a2ad4abf9176a10ba22fc05
Gilmarfilho2003 <89646761+Gilmarfilho2003@users.noreply.github.com>
2022-01-15 17:10:19 -0300

    Update coibase.py

--- coibase.py
+++ coibase.py
@@ -10,7 +10,7 @@ if sys.version_info[0] != 3:
 
 PASSWORD_FILE = "passwords.txt"
 MIN_PASSWORD_LENGTH = 7
-GET_URL = 'https://www.coinbase.com/signin?locale=pt'
+GET_URL = 'https://www.coinbase.com/signin'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
 }
